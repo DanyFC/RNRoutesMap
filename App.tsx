596ea1cfc517a98f78ae65fc0fcc08ffe5c4dd07
DanyFC <danyfarfanx@gmail.com>
2024-10-22 10:45:20 -0500

    chore: :bento: added font loading and splash screen functionality to App.tsx, updated app.json to include expo-font and expo-splash-screen plugins, and installed required dependencies in package.json

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+
+  const [loaded, error] = useFonts({
+    'Lato-Black': require('./assets/fonts/Lato-Black.ttf'),
+    'Lato-Bold': require('./assets/fonts/Lato-Bold.ttf'),
+    'Lato-Light': require('./assets/fonts/Lato-Light.ttf'),
+    'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
+  })
+
+  useEffect(() => {
+    if (loaded || error) SplashScreen.hideAsync()
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{fontSize: 20, fontFamily: 'Lato-Bold'}}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
